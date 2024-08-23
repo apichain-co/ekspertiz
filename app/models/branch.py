fa99ce20436b773402d9500a60e5842257b5cbf0
Ali Altun <92324915+alibilgealtun@users.noreply.github.com>
2024-08-24 00:19:08 +0300

    Create branch & company models

---
+++ app/models/branch.py
@@ -0,0 +1,16 @@
+from ..database import db
+
+
+class Branch(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)  # Branch name or identifier
+    company_id = db.Column(db.Integer, db.ForeignKey('company.id'), nullable=False)
+    address = db.Column(db.String(255))
+    phone_1 = db.Column(db.String(15))
+    phone_2 = db.Column(db.String(15))
+    fax = db.Column(db.String(15))
+    email = db.Column(db.String(100))
+    staff_members = db.relationship('Staff', backref='branch', lazy=True)
+
+    def __repr__(self):
+        return f'<Branch {self.name} - {self.company.name}>'
---
+++ app/models/company.py
@@ -0,0 +1,17 @@
+from ..database import db
+
+
+class Company(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    phone_1 = db.Column(db.String(15))
+    phone_2 = db.Column(db.String(15))
+    fax = db.Column(db.String(15))
+    email = db.Column(db.String(100))
+    website = db.Column(db.String(100))
+    address = db.Column(db.String(255))
+    my_business_address_link = db.Column(db.String(255))
+    branches = db.relationship('Branch', backref='company', lazy=True)
+
+    def __repr__(self):
+        return f'<Company {self.name}>'
