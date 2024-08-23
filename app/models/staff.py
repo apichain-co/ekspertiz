82915c4ea0b4524faf071f2eb9d94ca12ed3a121
Ali Altun <92324915+alibilgealtun@users.noreply.github.com>
2024-08-24 00:19:31 +0300

    Add role to staff

--- app/models/staff.py
+++ app/models/staff.py
@@ -1,11 +1,14 @@
 from ..database import db
 
+
 class Staff(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     full_name = db.Column(db.String(100), nullable=False)
     password = db.Column(db.String(100), nullable=False)
     phone_number = db.Column(db.String(15), nullable=False)
-    department = db.Column(db.String(50), nullable=False)  # manager, employee, etc.
+    department = db.Column(db.String(50), nullable=False)  # e.g., "IT", "Sales", etc.
+    role = db.Column(db.String(50), nullable=False)  # e.g., "Officer", "Manager", "Employee"
+    branch_id = db.Column(db.Integer, db.ForeignKey('branch.id'), nullable=False)
 
     def __repr__(self):
-        return f'<Personnel {self.full_name}>'
+        return f'<Staff {self.full_name} - {self.role}>'
