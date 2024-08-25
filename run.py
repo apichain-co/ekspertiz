efc189dc09852065db9ebb05628de69c1756136a
Ali Altun <92324915+alibilgealtun@users.noreply.github.com>
2024-08-25 04:38:43 +0300

    add filter

--- run.py
+++ run.py
@@ -2,5 +2,12 @@ from app import create_app
 
 app = create_app()
 
+
+@app.template_filter('skip_none')
+def skip_none(value):
+    return value if value is not None else ''
+
+
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port="5131", debug=True)
