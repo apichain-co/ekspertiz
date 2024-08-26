47b1f0f22de475a7ec89eb81b10b0f99a44ad1f9
Ali Altun <92324915+alibilgealtun@users.noreply.github.com>
2024-08-26 03:09:42 +0300

    add errors blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from .database import db, migrate
 from .models import appointment, customer, package, report, staff, branch, company
+import logging
+from logging.handlers import RotatingFileHandler
 
 # Import blueprints
 from .routes.appointments import appointments as appointments_bp
@@ -10,11 +12,19 @@ from .routes.staffs import staff as staff_bp
 from .routes.main import main as main_bp
 from .routes.branches import branches as branches_bp
 from .routes.companies import companies as companies_bp
-
+from .routes.errors import errors as errors_bp
 
 def create_app(config_object='config.Config'):
     app = Flask(__name__)
     app.config.from_object(config_object)
+
+    if not app.debug:
+        file_handler = RotatingFileHandler('logs/error.log', maxBytes=10240, backupCount=10)
+        file_handler.setLevel(logging.ERROR)
+        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s [in %(pathname)s:%(lineno)d]')
+        file_handler.setFormatter(formatter)
+        app.logger.addHandler(file_handler)
+
 
     db.init_app(app)
 
@@ -24,6 +34,7 @@ def create_app(config_object='config.Config'):
         db.create_all()
 
     # Register blueprints here
+    app.register_blueprint(errors_bp)
     app.register_blueprint(branches_bp)
     app.register_blueprint(companies_bp)
     app.register_blueprint(appointments_bp)
