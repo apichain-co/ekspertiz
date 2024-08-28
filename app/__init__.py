131f9fdd048dfe0bf77adecb9a0fe8ee6e0828db
Ali Altun <92324915+alibilgealtun@users.noreply.github.com>
2024-08-28 13:42:02 +0300

    update __init__

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,10 @@ from .database import db, migrate
 from .models import appointment, customer, package, report, staff, branch, company
 import logging
 from logging.handlers import RotatingFileHandler
+from .services.commands import register_commands
+from .services.expertise_initializer import ExpertiseInitializer
+from .services.package_initializer import create_standard_package
+
 
 # Import blueprints
 from .routes.appointments import appointments as appointments_bp
@@ -13,6 +17,9 @@ from .routes.main import main as main_bp
 from .routes.branches import branches as branches_bp
 from .routes.companies import companies as companies_bp
 from .routes.errors import errors as errors_bp
+from .routes.packages import packages as packages_bp
+
+
 
 def create_app(config_object='config.Config'):
     app = Flask(__name__)
@@ -25,15 +32,22 @@ def create_app(config_object='config.Config'):
         file_handler.setFormatter(formatter)
         app.logger.addHandler(file_handler)
 
-
     db.init_app(app)
 
     migrate.init_app(app, db)
 
     with app.app_context():
         db.create_all()
+        ExpertiseInitializer.initialize_expertise_reports()  # Initialize expertise reports
+        create_standard_package()
+        # print_package_reports("Standard Package")
+
+
+
+    register_commands(app)
 
     # Register blueprints here
+    app.register_blueprint(packages_bp)
     app.register_blueprint(errors_bp)
     app.register_blueprint(branches_bp)
     app.register_blueprint(companies_bp)
