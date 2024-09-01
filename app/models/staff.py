b9a1ded8024dc4335d07a4f13ef1c684f5a6e849
Ali Altun <92324915+alibilgealtun@users.noreply.github.com>
2024-09-02 02:27:50 +0300

    update staff temporarily

--- app/models/staff.py
+++ app/models/staff.py
@@ -4,12 +4,13 @@ from ..database import db
 class Staff(db.Model):
     __tablename__ = 'staff'
     id = db.Column(db.Integer, primary_key=True)
-    full_name = db.Column(db.String(100), nullable=False)
-    password = db.Column(db.String(100), nullable=False)
+    full_name = db.Column(db.String(100), nullable=False, unique=True)
+    password = db.Column(db.String(255), nullable=False)
     phone_number = db.Column(db.String(15), nullable=False)
-    department = db.Column(db.String(50), nullable=False)  # e.g., "IT", "Sales", etc.
+    department = db.Column(db.String(50), nullable=True)  # e.g., "IT", "Sales", etc.
     role = db.Column(db.String(50), nullable=False)  # e.g., "Officer", "Manager", "Employee"
-    branch_id = db.Column(db.Integer, db.ForeignKey('branch.id'), nullable=False)
+
+    branch_id = db.Column(db.Integer, db.ForeignKey('branch.id'), nullable=True)
     reports_created = db.relationship('Report', back_populates='staff', lazy=True, overlaps="staff_reports,staff")
 
     def __repr__(self):
